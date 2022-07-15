358c3b02212a7138fc84b0815a43a303cbf4e8e5
Lucas-Alipio <lucasantonio@gec.inatel.br>
2022-07-15 17:02:41 -0300

    :sparkles: feat: Add Side Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { AppRoutes } from "./routes";
+import { SideMenu } from "./shared/components";
 import { AppThemeProvider } from "./shared/contexts";
 
 export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes />
+
+        <SideMenu>
+          <AppRoutes />
+        </SideMenu>
+
       </BrowserRouter>
     </AppThemeProvider>
   );
